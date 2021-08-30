973b97b724d5df6504e3f6f2f8f135147ff1e6c9
Olivier Blanc <olivier3lanc@gmail.com>
2021-08-30 22:43:11 +0200

    WIP JS resizer

--- libdoc/js/template.js
+++ libdoc/js/template.js
@@ -111,3 +111,33 @@ jQuery(document).ready(function() {
         );
     }
 });
+
+var p = document.querySelector('#page-featured-playground-container > div');
+
+p.className = p.className + ' resizable';
+var resizer = document.createElement('div');
+resizer.className = 'resizer';
+p.appendChild(resizer);
+resizer.addEventListener('mousedown', initDrag, false);
+
+var startX, startY, startWidth, startHeight;
+
+function initDrag(e) {
+   startX = e.clientX;
+   startY = e.clientY;
+   startWidth = parseInt(document.defaultView.getComputedStyle(p).width, 10);
+   startHeight = parseInt(document.defaultView.getComputedStyle(p).height, 10);
+   document.documentElement.addEventListener('mousemove', doDrag, false);
+   document.documentElement.addEventListener('mouseup', stopDrag, false);
+}
+
+function doDrag(e) {
+   p.style.width = (startWidth + e.clientX - startX) + 'px';
+//    p.style.height = (startHeight + e.clientY - startY) + 'px';
+}
+
+function stopDrag(e) {
+    console.log(e.target);
+    document.documentElement.removeEventListener('mousemove', doDrag, false);    
+    document.documentElement.removeEventListener('mouseup', stopDrag, false);
+}
